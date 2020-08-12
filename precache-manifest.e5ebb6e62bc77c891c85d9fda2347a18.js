df2d07198d699b8f4a0c3e94ae8056600cd9b724
root <root@vps-38acb2ea.vps.ovh.us>
2020-08-12 04:38:33 +0000

    Updates

---
+++ precache-manifest.e5ebb6e62bc77c891c85d9fda2347a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838bda81c855c73915ba4c4405d0c9a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6512394a20e5ae67f162",
+    "url": "/static/css/main.f5b3610e.chunk.css"
+  },
+  {
+    "revision": "ba0f60bb9c70b8c71794",
+    "url": "/static/js/2.32727048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32727048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6512394a20e5ae67f162",
+    "url": "/static/js/main.62ec2121.chunk.js"
+  },
+  {
+    "revision": "6fba6406fd11ceadd132",
+    "url": "/static/js/runtime-main.4bb67260.js"
+  }
+]);
