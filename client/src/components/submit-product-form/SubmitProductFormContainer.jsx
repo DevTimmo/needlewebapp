facdceff563559183662c544a7cac757ffcf1192
tnoye <tnoye59044@my.sunywcc.edu>
2020-08-29 01:03:11 -0400

    Added: Folder management

---
+++ client/src/components/submit-product-form/SubmitProductFormContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../homepage/Header';
+
+function SubmitProductFormContainer() {
+
+    return (
+        <div>
+            <Header />
+            <p> Hello World! </p>
+        </div>
+    );
+}
+export default SubmitProductFormContainer;
