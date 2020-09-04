e31da6180d9f42af91c9a3cc95b4ddf2605c24c5
tnoye <tnoye59044@my.sunywcc.edu>
2020-09-04 01:18:00 -0400

    Added: images to header

--- client/src/components/homepage/Header.jsx
+++ client/src/components/homepage/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import SyringeImage from './SyringeImage';
+
 
 function Header() {
 
     return (
-        <h1><a href="/">Needle/Syringe Search Tool</a></h1>
+        <h1>
+            <SyringeImage />
+            <a href="/">Needle/Syringe Search Tool</a>
+            <img src='https://i.imgur.com/YH5VEIw.png' style={{ height: '60px', width: '150px', transform: 'rotate(180deg)' }} alt='a syringe' />
+        </h1>
     );
 }
 export default Header;
