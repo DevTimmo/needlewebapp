4332a47958434dbeab8699d2e8e2a53e11fb8811
tnoye <tnoye59044@my.sunywcc.edu>
2020-10-23 20:42:16 -0400

    Adding google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ReactGA from 'react-ga';
 import './App.css';
 import HomePage from './components/homepage/HomePageContainer'
 import SubmitProductFormContainer from './components/submit-product-form/SubmitProductFormContainer'
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('G-YXWKW8WJ3L');
+    ReactGA.pageview(window.location.pathname);
+  })
 
   return (
     <div className="App">
