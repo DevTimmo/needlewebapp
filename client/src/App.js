2132d98aa9819bfcf00e1b5e2caaf0971f914ea7
tnoye <tnoye59044@my.sunywcc.edu>
2020-10-23 21:11:27 -0400

    removed react router which isn't being used

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ReactGA from 'react-ga';
 import './App.css';
 import HomePage from './components/homepage/HomePageContainer'
-import SubmitProductFormContainer from './components/submit-product-form/SubmitProductFormContainer'
 
 
 function App() {
@@ -17,12 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/submitproduct" component={SubmitProductFormContainer} />
-        </Switch>
-      </Router>
+      <HomePage />
     </div>
   );
 };
